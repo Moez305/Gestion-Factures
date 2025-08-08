ea698b8b569ff00fc4fe58e845de9731a1d9f528
Moez305 <moez.bakini@sesame.com.tn>
2025-08-08 16:45:00 +0100

    Fix bill form layout and add missing NewClient route: Move payment checkbox to correct position and add NewClient page routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import ClientDetails from './pages/ClientDetails';
 import NewBill from './pages/NewBill';
+import NewClient from './pages/NewClient';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <nav className="navbar">
           <div className="container">
             <Link to="/">🏠 Home</Link>
+            <Link to="/new-client">👤 New Client</Link>
             <Link to="/new-bill">📄 New Bill</Link>
           </div>
         </nav>
@@ -20,6 +22,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/client/:id" element={<ClientDetails />} />
+            <Route path="/new-client" element={<NewClient />} />
             <Route path="/new-bill" element={<NewBill />} />
           </Routes>
         </div>
